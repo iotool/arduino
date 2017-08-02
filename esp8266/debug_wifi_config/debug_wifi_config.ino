1ff46a708b829481c7de94d5b45d75b25fc26581
iotool <roha.mbox@gmail.com>
2017-08-02 22:55:46 +0200

    Add files via upload

--- esp8266/debug_wifi_config/debug_wifi_config.ino
+++ esp8266/debug_wifi_config/debug_wifi_config.ino
@@ -1,4 +1,30 @@
-#ifdef ESP8266extern "C" {  #include "user_interface.h"}#endif
-// esp8266/config_store.c
-void setup() {  Serial.begin(9600);#ifdef ESP8266  struct softap_config wifiSoftapConfig;  struct station_config wifiStationConfig;  wifi_softap_get_config(&wifiSoftapConfig);   wifi_station_get_config(&wifiStationConfig);   String wifiSoftapConfig_ssid = (char*)wifiSoftapConfig.ssid;  String wifiSoftapConfig_password = (char*)wifiSoftapConfig.password;  String wifiStationConfig_ssid = (char*)wifiStationConfig.ssid;  String wifiStationConfig_password = (char*)wifiStationConfig.password;  Serial.println("AccessPoint");  Serial.print("wifiSoftapConfig.ssid[32]: "); Serial.println(wifiSoftapConfig_ssid);  Serial.print("wifiSoftapConfig.password[64]: "); Serial.println(wifiSoftapConfig_password);  Serial.println("Station");  Serial.print("wifiStationConfig.ssid[32]: "); Serial.println(wifiStationConfig_ssid);  Serial.print("wifiStationConfig.password[64]: "); Serial.println(wifiStationConfig_password);#endif}
-}}
+// https://github.com/iotool/arduino/esp8266/debug_wifi_config/debug_wifi_config.ino
+
+#ifdef ESP8266
+extern "C" {
+  #include "user_interface.h"
+}
+#endif
+
+void setup() {
+  Serial.begin(9600);
+#ifdef ESP8266
+  struct softap_config wifiSoftapConfig;
+  struct station_config wifiStationConfig;
+  wifi_softap_get_config(&wifiSoftapConfig); 
+  wifi_station_get_config(&wifiStationConfig); 
+  String wifiSoftapConfig_ssid = (char*)wifiSoftapConfig.ssid;
+  String wifiSoftapConfig_password = (char*)wifiSoftapConfig.password;
+  String wifiStationConfig_ssid = (char*)wifiStationConfig.ssid;
+  String wifiStationConfig_password = (char*)wifiStationConfig.password;
+  Serial.println("AccessPoint");
+  Serial.print("wifiSoftapConfig.ssid[32]: "); Serial.println(wifiSoftapConfig_ssid);
+  Serial.print("wifiSoftapConfig.password[64]: "); Serial.println(wifiSoftapConfig_password);
+  Serial.println("Station");
+  Serial.print("wifiStationConfig.ssid[32]: "); Serial.println(wifiStationConfig_ssid);
+  Serial.print("wifiStationConfig.password[64]: "); Serial.println(wifiStationConfig_password);
+#endif
+}
+
+void loop() {
+}
