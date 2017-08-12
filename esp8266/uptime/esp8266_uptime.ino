b78e5bb44ceac22fd9ed4cb4a4bfa12a9aca86ee
iotool <roha.mbox@gmail.com>
2017-08-13 00:15:51 +0200

    Add files via upload

---
+++ esp8266/uptime/esp8266_uptime.ino
@@ -0,0 +1,16 @@
+#include "espuptime.h"
+
+EspUptime uptime;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+int g_uptime = 0;
+
+void loop() {
+  uptime.update();
+  g_uptime = uptime.seconds();
+  delay(100);
+}
---
+++ esp8266/uptime/espuptime.h
@@ -0,0 +1,35 @@
+class EspUptime {
+  public:
+    EspUptime();
+    void update();
+    unsigned long seconds();
+  private:
+    unsigned long _seconds;
+    unsigned int _thisMillis;
+    unsigned int _lastMillis;
+};
+
+EspUptime::EspUptime() {
+  this->_seconds = 0;
+  this->_thisMillis = 0;
+  this->_lastMillis = 0;
+}
+
+void EspUptime::update() {
+  // must called once a minute
+  this->_thisMillis = (unsigned int)(millis() % 0xFFFF);
+  unsigned int deltaMillis = (unsigned int)(this->_thisMillis - this->_lastMillis);
+  if (deltaMillis >= 1000) {
+    this->_lastMillis = this->_thisMillis;
+    while (deltaMillis >= 1000) {
+      deltaMillis -= 1000;
+      this->_seconds++;
+    }
+  }
+}
+
+unsigned long EspUptime::seconds() {
+  // up to 136 years
+  return this->_seconds;
+}
+
