623a7605de9f9cfab31f7ede488da7ae50e427db
iotool <roha.mbox@gmail.com>
2017-10-24 20:21:28 +0200

    Add files via upload

--- atmega328p/SerialEcho9600/SerialEcho9600.ino
+++ atmega328p/SerialEcho9600/SerialEcho9600.ino
@@ -1,1 +1,21 @@
-// SerialEcho9600.ino
+// SerialEcho9600
+// 
+// Test driver for SoftwareSerialRepeaterLCD to display serial input on LCD.
+// 
+// PIN 00 = Serial.TX --- PIN 11 SoftwareSerial.RX
+// PIN 01 = Serial.RX --- PIN 10 SoftwareSerial.TX
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){;}
+  Serial.println();
+  Serial.println("SETUP");
+  delay(1000);
+}
+
+void loop() {
+  delay(1);
+  if (Serial.available()) {
+    Serial.write(Serial.read());
+  }
+}
