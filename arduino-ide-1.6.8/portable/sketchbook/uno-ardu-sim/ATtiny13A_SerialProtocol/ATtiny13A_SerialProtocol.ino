7b74886ab532fa9e18babeae29f12c58017c1a5a
iotool <roha.mbox@gmail.com>
2017-10-25 17:48:43 +0200

    POC Memory API

--- arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/ATtiny13A_SerialProtocol.ino
+++ arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/ATtiny13A_SerialProtocol.ino
@@ -1,10 +1,13 @@
 // ATtiny13A_SerialProtocol.ino
 // POC for UnoArduSim
+// 
+// 13 Byte SerialBuffer
+// 32 Byte MemoryBuffer
 
 #define SERIAL_BEGIN   9600
 #define SERIAL_TX      PB3
 #define SERIAL_RX      PB4
-#define SERIAL_BUFFER  16
+#define SERIAL_BUFFER  13
 #include "SoftwareSerial13A.h"
 
 void setup() {
@@ -21,6 +24,10 @@ void sendERR() {
 }
 
 #define AT_COMMAND_VERSION 'V'
+#define AT_COMMAND_MEMORY 'M'
+#define AT_COMMAND_MEMORY_CLEAR 'C'
+#define AT_COMMAND_MEMORY_GET 'G'
+#define AT_COMMAND_MEMORY_SET 'S'
 #define AT_MEMORY_BUFFER 32
 byte memoryBuffer[AT_MEMORY_BUFFER] = {0};
 
@@ -32,7 +39,35 @@ void loop() {
       SERIAL_PRINT(serialBuffer);
       if (serialBuffer[2]=='\n') {rc=1;} 
       else if (serialBuffer[2]=='+') {
-        if (serialBuffer[3] == AT_COMMAND_VERSION) {rc=1;SERIAL_PRINT("V:1\n");}
+        // AT
+        if (serialBuffer[4]=='\n' && serialBuffer[3]==AT_COMMAND_VERSION) {
+          SERIAL_PRINT("V:1.0\n"); 
+          rc=1; 
+        }
+        // AT+V
+        else if (serialBuffer[4]=='\n' && serialBuffer[3]==AT_COMMAND_MEMORY) {
+          SERIAL_PRINT("M:"); SERIAL_PRINT(AT_MEMORY_BUFFER); SERIAL_PRINT("\n"); 
+          rc=1;
+        }
+        // AT+M
+        else if (serialBuffer[5]=='\n' && serialBuffer[3]==AT_COMMAND_MEMORY && serialBuffer[4]==AT_COMMAND_MEMORY_CLEAR) {
+          for (rc=0; rc<AT_MEMORY_BUFFER; rc++) {memoryBuffer[rc]=0;} 
+          rc=1; 
+        }
+        // AT+MG00
+        else if (serialBuffer[7]=='\n' && serialBuffer[3]==AT_COMMAND_MEMORY && serialBuffer[4]==AT_COMMAND_MEMORY_GET) {
+          rc = (serialBuffer[5]-48)*10+(serialBuffer[6]-48);
+          SERIAL_PRINT("MG"); SERIAL_PRINT(rc); SERIAL_PRINT(":"); SERIAL_PRINT(memoryBuffer[rc]); SERIAL_PRINT("\n");
+          rc = 1;
+        }
+        // AT+MS00=123
+        else if (serialBuffer[7]=='=' && serialBuffer[3]==AT_COMMAND_MEMORY && serialBuffer[4]==AT_COMMAND_MEMORY_SET) {
+          rc = (serialBuffer[5]-48)*10+(serialBuffer[6]-48);
+          if (serialBuffer[9]=='\n')  {memoryBuffer[rc] = (serialBuffer[8]-48);} 
+          else if (serialBuffer[10]=='\n') {memoryBuffer[rc] = (serialBuffer[8]-48)*10+(serialBuffer[9]-48);} 
+          else {memoryBuffer[rc] = (serialBuffer[8]-48)*100+(serialBuffer[9]-48)*10+(serialBuffer[10]-48);} 
+          rc = 1;
+        }
       }
       if (rc == 1) {sendOK();} else {sendERR();}
     }
--- arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/SoftwareSerial13A.h
+++ arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/SoftwareSerial13A.h
@@ -9,13 +9,35 @@ void SERIAL_PRINT(const char* s) {
   Serial.print(s);
 }
 
+  byte n = 0; int m = 0;
+
+void SERIAL_PRINT(const int i) {
+  m = i;
+  for (n=0; n<sizeof(serialBuffer); n++) {
+    serialBuffer[n] = 0;
+  }
+  n = 0;
+  while(m>=10) {
+    serialBuffer[n] = m % 10;
+    m = (m - serialBuffer[n]) / 10;
+    serialBuffer[n] += 48;
+    n++;
+  }
+  serialBuffer[n] = 48 + m % 10;
+  n++;
+  while(n>0) {
+    n--;
+    Serial.print((char)serialBuffer[n]);
+  }
+}
+
 void SERIAL_READLN() {
   char serialRead = ' ';
   byte serialNoop = 255;
   serialPosition = 0;
   SERIAL_DATA = false;
   if (Serial.available()) {
-    while(((serialRead = (char)Serial.read()) != '\n') && (serialPosition < sizeof(serialBuffer)) && (serialNoop > 0)) {
+    while(((serialRead = (char)Serial.read()) != '\n') && (serialPosition < SERIAL_BUFFER) && (serialNoop > 0)) {
       if (serialRead >= 0) {
         serialBuffer[serialPosition] = serialRead;
         serialPosition++;
@@ -25,8 +47,8 @@ void SERIAL_READLN() {
         serialNoop--;
       }
     }
-    if ((serialNoop == 0) && (serialPosition+1 < sizeof(serialBuffer))) {
-      if (serialBuffer[serialPosition] != 'n') {
+    if ((serialNoop == 0) && (serialPosition < SERIAL_BUFFER)) {
+      if (serialBuffer[serialPosition] != '\n') {
         serialBuffer[serialPosition] = '\n';
       }
     }
