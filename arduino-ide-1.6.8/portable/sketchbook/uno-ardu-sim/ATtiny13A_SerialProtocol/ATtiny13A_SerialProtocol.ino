c4d4834bcda7da3ef7f69c3f4346089ed16aba99
iotool <roha.mbox@gmail.com>
2017-10-25 16:12:33 +0200

    Add files via upload

--- arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/ATtiny13A_SerialProtocol.ino
+++ arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/ATtiny13A_SerialProtocol.ino
@@ -1,1 +1,41 @@
 // ATtiny13A_SerialProtocol.ino
+// POC for UnoArduSim
+
+#define SERIAL_BEGIN   9600
+#define SERIAL_TX      PB3
+#define SERIAL_RX      PB4
+#define SERIAL_BUFFER  16
+#include "SoftwareSerial13A.h"
+
+void setup() {
+  Serial.begin(9600);
+  SERIAL_PRINT("HI\n");
+}
+
+void sendOK() {
+  SERIAL_PRINT("OK\n");
+}
+
+void sendERR() {
+  SERIAL_PRINT("ERR\n");
+}
+
+#define AT_COMMAND_VERSION 'V'
+#define AT_MEMORY_BUFFER 32
+byte memoryBuffer[AT_MEMORY_BUFFER] = {0};
+
+void loop() {
+  SERIAL_READLN();
+  if (SERIAL_DATA) {
+    if (serialBuffer[0]=='A' || serialBuffer[1]=='T') {
+      byte rc = 0;
+      SERIAL_PRINT(serialBuffer);
+      if (serialBuffer[2]=='\n') {rc=1;} 
+      else if (serialBuffer[2]=='+') {
+        if (serialBuffer[3] == AT_COMMAND_VERSION) {rc=1;SERIAL_PRINT("V:1\n");}
+      }
+      if (rc == 1) {sendOK();} else {sendERR();}
+    }
+  }
+}
+
--- arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/SoftwareSerial13A.h
+++ arduino-ide-1.6.8/portable/sketchbook/uno-ardu-sim/ATtiny13A_SerialProtocol/SoftwareSerial13A.h
@@ -1,2 +1,35 @@
 // SoftwareSerial13A.h
 // Equivalent syntax for UnoArduSim
+
+char serialBuffer[SERIAL_BUFFER] = {0};
+byte serialPosition = 0;
+bool SERIAL_DATA = false;
+
+void SERIAL_PRINT(const char* s) {
+  Serial.print(s);
+}
+
+void SERIAL_READLN() {
+  char serialRead = ' ';
+  byte serialNoop = 255;
+  serialPosition = 0;
+  SERIAL_DATA = false;
+  if (Serial.available()) {
+    while(((serialRead = (char)Serial.read()) != '\n') && (serialPosition < sizeof(serialBuffer)) && (serialNoop > 0)) {
+      if (serialRead >= 0) {
+        serialBuffer[serialPosition] = serialRead;
+        serialPosition++;
+        SERIAL_DATA = true;
+        serialNoop = 255;
+      } else {
+        serialNoop--;
+      }
+    }
+    if ((serialNoop == 0) && (serialPosition+1 < sizeof(serialBuffer))) {
+      if (serialBuffer[serialPosition] != 'n') {
+        serialBuffer[serialPosition] = '\n';
+      }
+    }
+  }
+}
+
